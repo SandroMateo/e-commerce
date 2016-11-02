b3d02ed19277bb8b57cf9a02e045b29e03f28c6e
Kevin Johnston and Sandro Alvarez <sandro@>
2016-11-02 14:31:40 -0700

    Add women item display

--- app/components/women-list.js
+++ app/components/women-list.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  womenArray: [],
+  itemsArray: null,
+  womenSort: Ember.computed('itemsArray', 'womenArray', function() {
+    this.set('womenArray', []);
+    this.set('itemsArray', this.get('items.content'));
+    console.log(this.get('items.content'));
+    for (var i = 0; i < this.get('itemsArray').length; i++) {
+      if (this.get('itemsArray')[i]._data.sex === "women") {
+        this.get('womenArray').pushObject(this.get('itemsArray')[i]._data);
+      }
+    }
+    return this.get('womenArray');
+  })
 });
