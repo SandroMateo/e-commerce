792de816797450bce783239a47e873f83d2e8b7c
Kevin Johnston and Sandro Alvarez <sandro@>
2016-11-03 09:10:08 -0700

    delete unneeded components and fix totalcost computed property in cart-list

--- app/components/cart-list.js
+++ app/components/cart-list.js
@@ -5,7 +5,7 @@ export default Ember.Component.extend({
   totalCost: Ember.computed(function() {
     var addTotal = 0;
     for(var i = 0; i < this.get('shoppingCart').items.length; i ++) {
-      addTotal += this.get('shoppingCart').items[i].price;
+      addTotal += this.get('shoppingCart').items[i].data.price;
     }
     return addTotal;
   })
